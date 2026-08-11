04d832fb3db7b3951a156d731d6fae40aecf1012
AnnapoornaK <annapoorna.kamath@codeandtheory.com>
2026-08-11 15:52:40 +0530

    Intalling navigation related code

---
+++ screens/RecentExpense.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+function RecentExpense() {
+  return (
+    <View>
+      <Text>RecentExpense</Text>
+    </View>
+  );
+}
+
+export default RecentExpense;
