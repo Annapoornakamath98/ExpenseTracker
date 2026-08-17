d9566426f7fd08beaed492a46e3aecf1dff9df69
AnnapoornaK <annapoorna.kamath@codeandtheory.com>
2026-08-17 18:47:03 +0530

    error handling

--- screens/RecentExpense.js
+++ screens/RecentExpense.js
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+import ErrorOverlay from "../components/UI/ErrorOverlay";
 import LoadingOverlay from "../components/UI/LoadingOverlay";
 
 import { getDateMinusDays } from "../util/date";
@@ -9,18 +10,27 @@ import { ExpensesContext } from "../store/expense-context";
 
 function RecentExpenses() {
   const [isFetching, setIsFetching] = useState(true);
+  const [error, setError] = useState();
   const expensesCtx = useContext(ExpensesContext);
 
   useEffect(() => {
     async function getExpenses() {
       setIsFetching(true);
-      const expenses = await fetchExpenses();
+      try {
+        const expenses = await fetchExpenses();
+        expensesCtx.setExpenses(expenses);
+      } catch (error) {
+        setError("Could not fetch expenses - please try again later!");
+      }
       setIsFetching(false);
-      expensesCtx.setExpenses(expenses);
     }
 
     getExpenses();
   }, []);
+
+  if (error && !isFetching) {
+    return <ErrorOverlay message={error} />;
+  }
 
   if (isFetching) {
     return <LoadingOverlay />;
